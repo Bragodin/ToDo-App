cd6cf9e98aa033a1b73267fc24788f52986ab5d3
unknown <admin@noteusr-12.main.nces.by>
2019-09-30 21:27:42 +0300

    makeup makeup

--- ToDo/src/app/app-routing.module.ts
+++ ToDo/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './core/main-page/main-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
